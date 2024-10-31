34872d6cd6d45c73d36f22f5ccf2e65faee407e1
Larry-444 <lap444@hotmail.com>
2024-10-30 21:33:15 -0400

    Added most of the code

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from savings_account import create_savings_account
+from cd_account import create_cd_account
 
 # Define the main function
 def main():
@@ -9,21 +11,30 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
+    savings_balance = float(input('What is your savings account balance? '))
+    savings_interest = float(input('What is your APR interest rate? Please enter a whole number '))
+    savings_maturity = float(input('What is your loan maturity rate in months? '))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"Updated savings balance: ${updated_savings_balance:.2f}, Interest earned: ${interest_earned:.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance = float(input('What is your cd account balance? '))
+    cd_interest = float(input('What is your APR interest rate? Please enter a whole number '))
+    cd_maturity = float(input('What is your loan maturity rate in months? '))
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"Updated CD balance: ${updated_cd_balance:.2f}, Interest earned: ${interest_earned:.2f}")
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
